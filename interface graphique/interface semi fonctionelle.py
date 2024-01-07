6cd18f2a7c4240cee31361a00f3d7a0710bc220c
CyanUnderscore <106006219+CyanUnderscore@users.noreply.github.com>
2024-01-07 20:37:26 +0100

    added start menu

--- interface graphique/interface semi fonctionelle.py
+++ interface graphique/interface semi fonctionelle.py
@@ -2,12 +2,23 @@ import pygame
 import sys
 
 
+
+class ConnectFour:
+    def __init__(self):
+        pygame.init()
+        #les constantes du programmeimport pygame
+import sys
+
+
+
 class ConnectFour:
     def __init__(self):
         pygame.init()
         #les constantes du programme
-
+        self.inGame = False
         self.largeur = 7
+        self.menuButtonColor = (140, 140, 140)
+        self.menuButtonRect = pygame.Rect(0, 0, 0, 0)
         self.hauteur = 6
         self.taile_plateau = 120
         self.rayon = self.taile_plateau // 2 - 5
@@ -87,8 +98,7 @@ class ConnectFour:
                     break
             if count >= 4:
                 return True
-        return False
-
+        return False     
 
     def changer_joueur(self):
         """
@@ -102,42 +112,104 @@ class ConnectFour:
         """
         self.joueur_actuel = 3 - self.joueur_actuel
 
+    def check_hover(self, rect):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        if rect.collidepoint(mouse_x, mouse_y):
+            self.menuButtonColor = (90, 90, 90)
+        else:
+            self.menuButtonColor = (110, 110, 110)
+    
+    def write_text(self, font_adress, text_content, font_size, pos_x, pos_y, inflate_x, inflate_y,bg_color, font_color, reactive, Menu_button):
+        font = pygame.font.Font(font_adress, font_size)
+        text = font.render(text_content, True,  font_color, bg_color )
+        rect = text.get_rect()
+        rect.width += inflate_x
+        rect.height += inflate_y
+        rect.center = (pos_x, pos_y)
+        text_rect = text.get_rect(center=rect.center)
+        if reactive:
+            self.check_hover(rect)
+        if Menu_button:
+            self.menuButtonRect = rect
+        pygame.draw.rect(self.screen, bg_color, rect)
+        self.screen.blit(text, text_rect)
+
     def run(self):
         """
         input:
         None
-        
-        boucle principale du jeux
+        32        boucle principale du jeux
         
         return:
         None
         """
+
         while not self.fin_partie:
-            #ferme la fenetre si on appui sur le boutton croix en haut ou sur la touche echap
+            # ferme la fenetre si on appui sur le boutton croix en haut ou sur la touche echap
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                elif event.type==pygame.KEYDOWN:
-                        if event.key==pygame.K_ESCAPE:
-                                pygame.quit()
-                                sys.exit()
-                #fait bouger le pion en haut
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    if self.menuButtonRect.collidepoint(mouse_x, mouse_y) and not self.inGame:
+                        self.inGame = True
+
+                # fait bouger le pion en haut
                 elif event.type == pygame.MOUSEMOTION:
-                    pygame.draw.rect(self.screen, self.fond, (0, 0, self.taille[0] , self.taille[1]))
-                    colone = event.pos[0] // self.taile_plateau
-                    pygame.draw.circle(self.screen, self.j1_couleur if self.joueur_actuel == 1 else self.j2_couleur,
-                                       (colone * self.taile_plateau + self.taile_plateau // 2, self.taile_plateau // 2), self.rayon)
-                #fait tomber le pion
+                    if self.inGame:
+                        pygame.draw.rect(self.screen, self.fond, (0, 0, self.taille[0], self.taille[1]))
+                        colone = event.pos[0] // self.taile_plateau
+                        pygame.draw.circle(self.screen, self.j1_couleur if self.joueur_actuel == 1 else self.j2_couleur,
+                                        (colone * self.taile_plateau + self.taile_plateau // 2, self.taile_plateau // 2), self.rayon)
+                        
+                # fait tomber le pion
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    colone = event.pos[0] // self.taile_plateau
-                    ligne= event.pos[1] // self.taile_plateau
-                    if self.placer_pion(colone):
-                        if self.chercher_gagnant(ligne, colone):
-                            self.fin_partie = True
-                        else:
-                            self.changer_joueur()
-            self.dessiner_plateau()
+                    if self.inGame:
+                        colone = event.pos[0] // self.taile_plateau
+                        ligne = event.pos[1] // self.taile_plateau
+                        if self.placer_pion(colone):
+                            if self.chercher_gagnant(ligne, colone):
+                                self.fin_partie = True
+                            else:
+                                self.changer_joueur()
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    if self.menuButtonRect.collidepoint(mouse_x, mouse_y) and not self.inGame:
+                        self.inGame = True
+
+            if self.inGame:
+                self.dessiner_plateau()
+
+            else:
+                self.screen.fill(self.fond)
+                pygame.draw.rect(self.screen, (125, 125, 125), ((self.taille[0] - 0.4 * self.taille[0]) // 2, (self.taille[1] - 0.5 * self.taille[1]) // 2, 0.4 * self.taille[0], 0.5 * self.taille[1]))
+                
+                self.write_text(font_adress="interface_graphique/font.TTF",
+                                text_content="Play",
+                                font_size=48,
+                                pos_x=self.taille[0] // 2,
+                                pos_y=self.taille[1] // 2,
+                                inflate_x=100,
+                                inflate_y=50,
+                                bg_color=self.menuButtonColor,
+                                font_color=(0, 0, 0),
+                                reactive=True,
+                                Menu_button=True)
+                
+                self.write_text(font_adress="interface_graphique/font.TTF",
+                                text_content="Puissance 4",
+                                font_size=56,
+                                pos_x=self.taille[0] // 2,
+                                pos_y=self.taille[1] // 2 - self.taille[1] // 4,
+                                inflate_x=100,
+                                inflate_y=50,
+                                bg_color=(255, 0, 0),
+                                font_color=(255, 255, 0),
+                                reactive=False,
+                                Menu_button=False)
             pygame.display.flip()
             self.clock.tick(self.fps)
 
