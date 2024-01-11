df57c74e19e8d5522bb095dfa64703111eb74843
michael <mm.michaelmarion@gmail.com>
2024-01-11 10:37:19 +0100

    hm

--- ia/ia.py
+++ ia/ia.py
@@ -11,9 +11,9 @@ class Ia:
     def coup(self, joueur: bool, colonne: int) -> None:
         """Appeler cette fonction avec le coup effectué par un joueur pour mettre à jour l'état interne du plateau de l'IA.
         Le coup doit être valide."""
-        assert False, "Ia est une classe de base, il faut construire une classe enfante"
+        assert False, "Ia est une classe de base, il faut construire une classe enfant"
         
     def prediction(self) -> int | None:
         """Renvoie l'endroit sur lequel l'IA va jouer, ou `None` si aucun coup n'est possible,
         Ne pas oublier d'appeller `coup` avec le résultat de cette fonction si il n'est pas `None`"""
-        assert False, "Ia est classe de base, il faut construire une classe enfante"
+        assert False, "Ia est classe de base, il faut construire une classe enfant"
