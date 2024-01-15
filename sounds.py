669d022eb65b372a1db9d8fef84e1f2b7c6b5b87
Baronsabron <153923361+Baronsabron@users.noreply.github.com>
2024-01-15 17:51:12 +0100

    Add files via upload

--- sounds.py
+++ sounds.py
@@ -6,6 +6,8 @@ class Sound:
         pygame.mixer.init()  # Initialiser le module mixer
         self.pion_sound = pygame.mixer.Sound("pion.mp3")
         self.pion_sound.set_volume(0.5)
+        self.final_sound = pygame.mixer.Sound("fin.mp3")
+        self.final_sound.set_volume(1)
         self.music = 0
 
     def pion(self):
@@ -15,13 +17,16 @@ class Sound:
         if mode == 0: # Chargez la musique du jeu
             pygame.mixer.music.load("fondsonore.mp3")
             pygame.mixer.music.play(loops=-1, start=3)
-            pygame.mixer.music.set_volume(0.20)
+            pygame.mixer.music.set_volume(0.15)
         else:
             pygame.mixer.music.load("ia_hard.mp3")
             pygame.mixer.music.play(loops=-1, start=1.25)
-            pygame.mixer.music.set_volume(0.25)
+            pygame.mixer.music.set_volume(0.10)
 
     def jouer_musique_menu(self):
         pygame.mixer.music.load("menu.mp3")  # Chargez la musique du menu
         pygame.mixer.music.play(loops=-1)
-        pygame.mixer.music.set_volume(0.75)
+        pygame.mixer.music.set_volume(0.5)
+
+    def final(self):
+        self.final_sound.play()
