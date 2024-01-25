bfaf6cad9e40793f0e9be82b9c5caf364e561879
michael <mm.michaelmarion@gmail.com>
2024-01-25 10:22:48 +0100

    Dossier mp3

--- sounds.py
+++ sounds.py
@@ -4,12 +4,12 @@ class Sound:
 
     def __init__(self):
         pygame.mixer.init()  # Initialiser le module mixer
-        self.pion_sound = pygame.mixer.Sound("pion.mp3")
+        self.pion_sound = pygame.mixer.Sound("dossier_mp3/pion.mp3")
         self.pion_sound.set_volume(0.5)
-        self.final_sound = pygame.mixer.Sound("fin.mp3")
+        self.final_sound = pygame.mixer.Sound("dossier_mp3/fin.mp3")
         self.final_sound.set_volume(1)
         self.music = 0
-        self.bruh = pygame.mixer.Sound("bruh.mp3")
+        self.bruh = pygame.mixer.Sound("dossier_mp3/bruh.mp3")
         self.bruh.set_volume(1)
 
 
@@ -18,11 +18,11 @@ class Sound:
 
     def jouer_musique_jeu(self, mode, dif):
         if mode == 0: # Chargez la musique du jeu
-            pygame.mixer.music.load("fondsonore.mp3")
+            pygame.mixer.music.load("dossier_mp3/fondsonore.mp3")
             pygame.mixer.music.play(loops=-1, start=3)
             pygame.mixer.music.set_volume(0.15)
         else:
-            musique = ("easy.mp3", "medium.mp3", "hard.mp3", "demoniaque.mp3")
+            musique = ("dossier_mp3/easy.mp3", "dossier_mp3/medium.mp3", "dossier_mp3/hard.mp3", "dossier_mp3/demoniaque.mp3")
             volume = (0.15, 0.20, 0.15, 0.15)
             pygame.mixer.music.load(musique[dif])
             pygame.mixer.music.play(loops=-1, start=1.25)
@@ -30,7 +30,7 @@ class Sound:
 
 
     def jouer_musique_menu(self):
-        pygame.mixer.music.load("menu.mp3")  # Chargez la musique du menu
+        pygame.mixer.music.load("dossier_mp3/menu.mp3")  # Chargez la musique du menu
         pygame.mixer.music.play(loops=-1)
         pygame.mixer.music.set_volume(0.5)
 
